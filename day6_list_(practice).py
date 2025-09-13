299b20ce09bae2b1a3e5b10b8052dbee5b80a314
Section33-ops <saeedwumbei33@gmail.com>
2025-09-13 18:14:23 -0400

    Add loop practice(Day 5) python file

---
+++ day6_list_(practice).py
@@ -0,0 +1,7 @@
+# 09/13/2025
+# Practice: Create a list of your 5 favorite foods. Print them one by one.
+
+fav_food = ["Steak", "Grapes", "Fries", "Fried Chicken", "Chocolate"]
+
+for food in fav_food:
+    print(food)
