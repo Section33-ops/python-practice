e63ca8aea27bca06b6a87d181c320c40c7a0eba0
Section33-ops <saeedwumbei33@gmail.com>
2025-09-13 18:48:47 -0400

    Add functions practice(Day 7)

---
+++ day7_functions_(practice).py
@@ -0,0 +1,9 @@
+# 09/13/2025
+# Practice: Write a function greet(name) that prints "Hello,[name]"
+
+def greet(name):
+    print(f"Hello, {name}")
+
+name = input("What is your name? ")
+
+greet(name)
