ad3ec6a6f94f0ffc6bf15a8b0d5da43a9eaf0529
Section33-ops <saeedwumbei33@gmail.com>
2025-09-13 13:09:00 -0400

    Added a Python file which contains my variables & data types practice

---
+++ day2_variables_&_data_types_(Practice).py
@@ -0,0 +1,8 @@
+# 09/13/2025
+# Practice: Variables % data types(int, str, float)
+
+usr_name = "Paul"
+usr_age = 21
+usr_height = 63.5
+
+print(f"{usr_name} is {usr_age} years old and is {usr_height} inches tall")
