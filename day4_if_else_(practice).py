bed35f4477005b6a4a038b74deb675425de91f0f
Section33-ops <saeedwumbei33@gmail.com>
2025-09-13 17:55:28 -0400

    Add if/else practice(Day 4) python file

---
+++ day4_if_else_(practice).py
@@ -0,0 +1,8 @@
+# 09/13/2025
+# Practice: Program that checks if your age is >= 18, "You're an adult" else "You're a minor"
+
+usr_age = int(input("What is your age? "))
+if usr_age >= 18:
+    print("You're an adult")
+else:
+    print("You're a minor")
