ecedfd0527ce92cb8408810eea0f6cada7bea39f
Section33-ops <saeedwumbei33@gmail.com>
2025-09-13 17:00:03 -0400

    Add a Python practice file which contains my user input practice

---
+++ day3_user_input_(practice).py
@@ -0,0 +1,7 @@
+# 09/13/2025
+# Practice: Create a program that asks for your name and age, then prints it
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+print(f"Your name is {name} and you are {age} years old")
