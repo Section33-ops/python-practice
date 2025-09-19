47c035b6e47fe3e7ee3635bc1fdd7136731b2f87
Section33-ops <saeedwumbei33@gmail.com>
2025-09-19 06:35:01 -0400

    Add review basics practice part1(Day 9)

---
+++ day9_review_basics(practice).py
@@ -0,0 +1,9 @@
+# 09/14/2025
+# Variables Exrecise
+# We check in a patient named John Smith.
+# He is 20 years old.
+# He is a new patient.
+
+patient_name = "John Smith"
+patient_age = 20
+is_new_patient = True
