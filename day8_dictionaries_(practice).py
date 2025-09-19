d80dc582d031804312eed679e394cc1df96cf387
Section33-ops <saeedwumbei33@gmail.com>
2025-09-19 06:28:26 -0400

    Add dictionaries practice(Day 8)

---
+++ day8_dictionaries_(practice).py
@@ -0,0 +1,13 @@
+# 09/14/2025
+# Practice: Store 3 friends' names and ages in a dictionary. Print them.
+
+friends = {
+    "James": 13,
+    "Noah": 15,
+    "John": 16
+}
+
+
+# Print each friends name and age individually
+for name, age in friends.items():
+    print(f"{name} is {age} years old.")
