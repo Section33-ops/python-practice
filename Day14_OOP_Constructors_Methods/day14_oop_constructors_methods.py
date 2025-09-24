31b36f21abd34a8474f6a14dee939140cb07de92
Section33-ops <saeedwumbei33@gmail.com>
2025-09-24 06:54:41 -0400

    Add to day 14 python practice file

--- Day14_OOP_Constructors_Methods/day14_oop_constructors_methods.py
+++ Day14_OOP_Constructors_Methods/day14_oop_constructors_methods.py
@@ -1,3 +1,15 @@
 # 09/23/2025
 # Practice: Create a Car class with make, model, and year
 #           Add a method drive() that prints “The car is driving.”
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+    
+    def drive(self):
+        print(f"{self.make} {self.model} is driving.")
+
+car1 = Car("Honda", "Accord", "2015") 
+car1.drive()
