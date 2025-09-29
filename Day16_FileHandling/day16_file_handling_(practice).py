eade85439bdbfe50c488d462610cdffeb4155a41
Section33-ops <saeedwumbei33@gmail.com>
2025-09-29 06:32:19 -0400

    Practice file handling Day16

--- Day16_FileHandling/day16_file_handling_(practice).py
+++ Day16_FileHandling/day16_file_handling_(practice).py
@@ -1,3 +1,11 @@
 # 09/27/2025
 # Practice: Write a program thats asks the user for their names & saves it to names.txt
 #           Then read and display all names stored in the file
+
+usr_name = input("What is your name? ")
+
+with open("names.txt", "a") as name_file:
+    name_file.write(f"{usr_name}\n")
+
+with open("names.txt") as name_file:
+    print(name_file.read())
