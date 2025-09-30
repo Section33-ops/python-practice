0b23943ee59d75647f8827336a8821a79012c555
Section33-ops <saeedwumbei33@gmail.com>
2025-09-30 11:47:14 -0400

    Add day 18 modules and packages practice

---
+++ Day18_Modules_Packages/main.py
@@ -0,0 +1,7 @@
+# 09/30/2025
+# Practice: Write your own module greetings.py with a function say_hello(name)
+#           Import it into another another script and use it
+
+import greetings
+
+greetings.say_hello("Paul")
